855019b909906c3d9662b6ecccd1ab841c132d5b
Andres Cabrera <mantaraya36@gmail.com>
2022-07-26 20:13:42 -0700

    Initial commit

---
+++ frameworks/Wiring/1.0/project/wiring_project/wiring_project.ino
@@ -0,0 +1,22 @@
+
+//[[Includes]]
+
+//[[/Includes]]
+
+//[[Declarations]]
+//[[/Declarations]]
+
+
+void setup() {
+//[[Initialization]]
+//[[/Initialization]]
+}
+
+
+void loop() {
+//[[Processing]]
+//[[/Processing]]
+
+//[[Cleanup]]
+//[[/Cleanup]]
+}
